4980d03cd271d2a7be443421c9825efc14536ab4
Artem <agaleev583@gmail.com>
2024-11-29 17:05:45 +0300

    feat:add const

---
+++ script.js
@@ -0,0 +1,11 @@
+const startButtonSelector = ".button__start";
+const stopButtonSelector = ".button__stop";
+const hoursInputSelector = "#hours";
+const minutesInputSelector = "#minutes";
+const secondsInputSelector = "#seconds";
+
+const hoursInput = document.querySelector(hoursInputSelector);
+const minutesInput = document.querySelector(minutesInputSelector);
+const secondsInput = document.querySelector(secondsInputSelector);
+const startButton = document.querySelector(startButtonSelector);
+const stopButton = document.querySelector(stopButtonSelector);
