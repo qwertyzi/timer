546498e2decca86c80feef296f435b4b5db3f8c7
Artem <agaleev583@gmail.com>
2024-12-02 16:50:27 +0300

    feat:buttons.

--- script.js
+++ script.js
@@ -9,3 +9,54 @@ const minutesInput = document.querySelector(minutesInputSelector);
 const secondsInput = document.querySelector(secondsInputSelector);
 const startButton = document.querySelector(startButtonSelector);
 const stopButton = document.querySelector(stopButtonSelector);
+
+let remainingTime;
+let hours;
+let minute;
+let second; 
+let intervalId;
+
+const startTimer = (event) => {
+    event.preventDefault();
+    hours = parseInt(hoursInput.value);
+    minute = parseInt(minutesInput.value);
+    second = parseInt(secondsInput.value);
+
+    remainingTime = hours * 3600 + minute * 60 + second;
+
+    intervalId = setInterval(updateTimer, 1000)
+    document.documentElement.requestFullscreen();
+    hideElement(startButton);
+    showElement(stopButton);
+
+}
+
+const updateTimer = () => {
+    remainingTime =remainingTime -1;
+    hours = Math.floor(remainingTime / 3600);
+    minute = Math.floor(remainingTime % 3600 / 60);
+    second = remainingTime % 60;
+
+    hoursInput.value = hours;
+    minutesInput.value = minute;
+    secondsInput.value = second;
+
+}
+
+const stopTimer = () => {
+    clearInterval(intervalId);
+    document.exitFullscreen();
+    hideElement(stopButton);
+    showElement(startButton);
+}
+
+const hideElement = (element) => {
+    element.classList.add("hide");
+}
+
+const showElement = (element) => {
+    element.classList.remove("hide");
+}
+
+startButton.addEventListener('click', startTimer);
+stopButton.addEventListener('click', stopTimer);
